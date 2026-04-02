0c2a3e67838cafd0ff5b2bb8f6df18674e3df970
ArnavKala <akala8606@gmail.com>
2026-04-02 22:58:45 +0530

    Adding Post Entity

---
+++ src/main/java/com/course/rest/webservices/restful_web_services/jpa/PostRepository.java
@@ -0,0 +1,9 @@
+package com.course.rest.webservices.restful_web_services.jpa;
+
+import com.course.rest.webservices.restful_web_services.user.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Integer> {
+
+
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,16 @@ values(10002, current_date(),'Paratha');
 
 insert into user_details(id,birth_date,name)
 values(10003, current_date(),'Apple');
+
+insert into post(id,description,user_id)
+values(20001,'I want a samosa',10001);
+
+insert into post(id, description, user_id)
+values(20002,'I want a Paratha',10001);
+
+insert into post(id, description, user_id)
+values(20003,'I want an Apple',10002);
+
+insert into post(id, description, user_id)
+values(20004,'I want a Green Apple',10002);
+
